26e95c766c547ebb161a1d9980c5b103d3d129e3
Robin de Zwart <robindezwart11@gmail.com>
2022-04-25 04:48:04 -0700

    Dynamically calculate dice to be used in total

--- js/row.js
+++ js/row.js
@@ -6,9 +6,45 @@ function calcRowNums() {
     })
 }
 
+function calcRowActive() {
+    // Loop through each row in table body
+    $("table tbody tr").each(function () {
+        // Get all columns from this row
+        let curRow = $(this);
+        let children = curRow.children();
+
+        // Get dice results and roll type
+        let dice1 = children.eq(2);
+        let dice2 = children.eq(3);
+        let rollType = children.eq(5);
+
+        // Highlight which roll was used
+        switch (rollType.html()) {
+            case "Advantage":
+                if (parseInt(dice1.html()) >= parseInt(dice2.html()))
+                    dice1.addClass("table-active");
+                else
+                    dice2.addClass("table-active");
+                break;
+
+            case "Disadvantage":
+                if (parseInt(dice1.html()) <= parseInt(dice2.html()))
+                    dice1.addClass("table-active");
+                else
+                    dice2.addClass("table-active");
+                break;
+
+            default:
+                dice1.addClass("table-active");
+                break;
+        }
+    });
+}
+
 
 function refreshTable() {
     calcRowNums();
+    calcRowActive();
 }
 
 $(function () {
