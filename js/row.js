dd7745af13980163ce47c8b0f94da2950cce77cd
Robin de Zwart <robindezwart11@gmail.com>
2022-04-25 04:47:42 -0700

    Dynamically calculate row numbers

--- js/row.js
+++ js/row.js
@@ -1,7 +1,14 @@
-function calcRows() {
-    let rows = $(".rowNum");
+function calcRowNums() {
+    // Loop through each row in table body
+    $("table tbody tr").each(function (i) {
+        // Fill in row number
+        $(this).children().eq(0).html(i);
+    })
+}
+
 
 function refreshTable() {
+    calcRowNums();
 }
 
 $(function () {
