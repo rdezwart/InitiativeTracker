314128267e6b52392b11e0c694c77c4e7100f621
Robin de Zwart <robindezwart11@gmail.com>
2022-04-27 03:27:46 -0700

    Add functionality to row deletion form

--- js/form.js
+++ js/form.js
@@ -1,6 +1,42 @@
+function updateMaxIndex() {
+    let numRows = $("#charTable tbody tr").length;
+    let deleteIndex = $("#deleteIndex");
+    let deleteButton = $("#deleteButton");
+
+    let rollButton = $("#rollButton");
+    let sortButton = $("#sortButton");
+    let bothButton = $("#bothButton");
+
+    if (numRows === 0) {
+        deleteIndex.prop("disabled", true);
+        deleteButton.prop("disabled", true);
+
+        rollButton.prop("disabled", true);
+        sortButton.prop("disabled", true);
+        bothButton.prop("disabled", true);
+    } else {
+        deleteIndex.attr("max", numRows - 1);
+
+        deleteIndex.prop("disabled", false);
+        deleteButton.prop("disabled", false);
+
+        rollButton.prop("disabled", false);
+        sortButton.prop("disabled", false);
+        bothButton.prop("disabled", false);
+    }
+
+    if (deleteIndex.val() > deleteIndex.attr("max")) {
+        deleteIndex.val(deleteIndex.val() - 1);
+    }
+}
 
 // Driver
 $(function () {
+    updateMaxIndex();
+
+    let deleteIndex = $("#deleteIndex");
+    deleteIndex.val(deleteIndex.attr("max"));
+
     // Button listeners
 
     $("#rollButton").click(function () {
@@ -44,7 +80,20 @@ $(function () {
         );
 
         refreshTable();
+        updateMaxIndex();
         // console.log(data);
     });
+
+    $("#deleteForm").submit(function (e) {
+        e.preventDefault();
+        let data = $(this).serializeArray().reduce(function (obj, item) {
+            obj[item.name] = item.value;
+            return obj;
+        }, {});
+        // console.log(data);
+
+        $("#charTable tbody tr").eq(data["index"]).remove();
+        refreshTable();
+        updateMaxIndex();
     });
 })
