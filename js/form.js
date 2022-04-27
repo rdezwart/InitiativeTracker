dfb132d86dd16b6904362e59b8b48ae8a2dd3dfb
Robin de Zwart <robindezwart11@gmail.com>
2022-04-27 03:26:46 -0700

    Move event listeners inside document ready

--- js/form.js
+++ js/form.js
@@ -1,24 +1,29 @@
-// Event listener
-$("#rollButton").click(function () {
-    rollRows();
-    refreshTable();
-    updateTotals();
-});
 
-$("#sortButton").click(function () {
-    sortTable();
-    calcRowNums();
-});
-
-$("#bothButton").click(function () {
-    rollRows();
-    refreshTable();
-    updateTotals();
-    sortTable();
-    calcRowNums();
-});
-
+// Driver
 $(function () {
+    // Button listeners
+
+    $("#rollButton").click(function () {
+        rollRows();
+        refreshTable();
+        updateTotals();
+    });
+
+    $("#sortButton").click(function () {
+        sortTable();
+        calcRowNums();
+    });
+
+    $("#bothButton").click(function () {
+        rollRows();
+        refreshTable();
+        updateTotals();
+        sortTable();
+        calcRowNums();
+    });
+
+    // Form listeners
+
     $("#charForm").submit(function (e) {
         e.preventDefault();
         let data = $(this).serializeArray().reduce(function (obj, item) {
@@ -39,6 +44,7 @@ $(function () {
         );
 
         refreshTable();
-        console.log(data);
+        // console.log(data);
+    });
     });
 })
